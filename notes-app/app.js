fa59f3cfa793ebf3811d9ded92c47841d1b5811b
Georgi <georgy.keranov@gmail.com>
2022-04-19 15:09:43 +0300

    Add chalk package and use it for colors in console

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,13 @@
 import validator from 'validator';
+import chalk from 'chalk';
 import {email, note} from './notes.js';
 import fs from 'fs';
 
 if (validator.isEmail(email)) {
     fs.appendFileSync('notes.txt', email + ' wrote:\n');
     fs.appendFileSync('notes.txt', note + '\n');
+
+    console.log(chalk.green('Success!'));
+} else {
+    console.log(chalk.red('Error, please make sure that the email is correct!'));
 }
