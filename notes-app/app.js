596250414d5b83e3a042200dc570cd00bd6beb19
Georgi <georgy.keranov@gmail.com>
2022-04-19 19:33:41 +0300

    Clean exercises

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,13 +1,1 @@
-import validator from 'validator';
 import chalk from 'chalk';
-import {email, note} from './notes.js';
-import fs from 'fs';
-
-if (validator.isEmail(email)) {
-    fs.appendFileSync('notes.txt', email + ' wrote:\n');
-    fs.appendFileSync('notes.txt', note + '\n');
-
-    console.log(chalk.green('Success!'));
-} else {
-    console.log(chalk.red('Error, please make sure that the email is correct!'));
-}
--- notes-app/notes.js
+++
@@ -1,4 +0,0 @@
-const email = 'example@example.com';
-const note = 'Example note';
-
-export { email, note };
