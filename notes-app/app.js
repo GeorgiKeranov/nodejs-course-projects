d1f56eed8fb4adc89d9a96da4bc1726ec5545dc9
Georgi <georgy.keranov@gmail.com>
2022-04-21 11:42:07 +0300

    Add all of the required commands

--- notes-app/app.js
+++ notes-app/app.js
@@ -7,4 +7,16 @@ yargsHelper.command('add', 'Add a note', function() {
     console.log('Adding a note');
 });
 
+yargsHelper.command('remove', 'Remove a note', function() {
+    console.log('Removing a note');
+});
+
+yargsHelper.command('read', 'Read a note', function() {
+    console.log('Reading a note');
+});
+
+yargsHelper.command('list', 'List the notes', function() {
+    console.log('Listing the notes');
+});
+
 console.log(yargsHelper.argv);
