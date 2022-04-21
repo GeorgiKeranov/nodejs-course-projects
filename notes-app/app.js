82c40502772dc6a182a9610dcfbc2bb01b09a742
Georgi <georgy.keranov@gmail.com>
2022-04-21 11:30:47 +0300

    Add a test command to yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,1 +1,10 @@
 import chalk from 'chalk';
+import yargs from 'yargs';
+
+const yargsHelper = yargs(process.argv.slice(2)); 
+
+yargsHelper.command('add', 'Add a note', function() {
+    console.log('Adding a note');
+});
+
+console.log(yargsHelper.argv);
