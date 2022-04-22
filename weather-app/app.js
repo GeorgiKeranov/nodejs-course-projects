cfbee947c769b8feae53bef6aede19ae32ae6995
Georgi <georgy.keranov@gmail.com>
2022-04-22 18:08:49 +0300

    Extract current temperature and current temperature feels like data from api

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,12 @@
 const request = require('request');
 
-const apiUrl = 'http://api.weatherstack.com/current?access_key=8dd121b76715aba773d70afa5db41a05&query=New%20York';
+const apiUrl = 'http://api.weatherstack.com/current?access_key=8dd121b76715aba773d70afa5db41a05&query=Sofia,Bulgaria';
 
 request({ url: apiUrl, json: true }, (error, response) => {
-    console.log(response.body.current);
+    const currentWeather = response.body.current;
+
+    const currentTemperature = currentWeather.temperature;
+    const currentTemperatureFeelsLike = currentWeather.feelslike;
+
+    console.log(`It is currently ${currentTemperature} degrees out but if feels like a ${currentTemperatureFeelsLike}.`);
 });
