f7af86d2945c4f2d321e9dd7c7b11c58676323c6
Georgi <georgy.keranov@gmail.com>
2022-04-22 18:50:10 +0300

    Integrate with another one API to search for geolocation cordinates by city and country name

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,12 +1,32 @@
 const request = require('request');
 
-const apiUrl = 'http://api.weatherstack.com/current?access_key=8dd121b76715aba773d70afa5db41a05&query=Sofia,Bulgaria';
+// Get the console arguments
+const consoleArguments = process.argv;
+// Remove the first two elements because they are the path and the name of the file
+consoleArguments.splice(0, 2);
+// Combine all of the left arguments from console
+const placeToCheckTheWeather = consoleArguments.join(' ');
 
-request({ url: apiUrl, json: true }, (error, response) => {
-    const currentWeather = response.body.current;
+const mapboxApiUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + placeToCheckTheWeather.replace(' ', '%20') +
+    '.json?access_token=pk.eyJ1IjoiZ2tlcmFub3YiLCJhIjoiY2wyYWt2dzJrMDBtYjNjcnozYnczaWtpciJ9.rOci68dTvbiZzIVPKznnXw';
 
-    const currentTemperature = currentWeather.temperature;
-    const currentTemperatureFeelsLike = currentWeather.feelslike;
-
-    console.log(`It is currently ${currentTemperature} degrees out but if feels like a ${currentTemperatureFeelsLike}.`);
+request({ url: mapboxApiUrl, json: true }, (error, response) => {
+    const responseBody = response.body;
+    const placeCordinates = responseBody.features[0].center;
+    const lng = placeCordinates[0];
+    const lat = placeCordinates[1];
+    
+    const weatherApiUrl = 'http://api.weatherstack.com/current?access_key=8dd121b76715aba773d70afa5db41a05&query=' + lat +',' + lng;
+    
+    request({ url: weatherApiUrl, json: true }, (error, response) => {
+        const currentWeather = response.body.current;
+    
+        const currentTemperature = currentWeather.temperature;
+        const currentTemperatureFeelsLike = currentWeather.feelslike;
+        const weatherDescription = currentWeather.weather_descriptions[0];
+    
+        console.log(`It is ${weatherDescription} here in ${placeToCheckTheWeather}.`);
+        console.log(`It is currently ${currentTemperature} degrees out.`);
+        console.log(`It feels like a ${currentTemperatureFeelsLike} degrees.`);
+    });
 });
