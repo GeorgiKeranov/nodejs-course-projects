a302dcfb20cd37395943cae14f839773df49edaa
Georgi <georgy.keranov@gmail.com>
2022-04-23 18:06:02 +0300

    Add client side javascript to fetch data from our weather api endpoint

--- web-server/public/js/main.js
+++ web-server/public/js/main.js
@@ -1,1 +1,10 @@
-console.log('Hello from client side javascript!');
+fetch('http://localhost:3000/weather?address=Sofia%20Bulgaria')
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
