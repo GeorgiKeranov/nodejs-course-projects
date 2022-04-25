ac5b69c73332443010378fec086052d98a01173d
Georgi <georgy.keranov@gmail.com>
2022-04-25 22:21:24 +0300

    Refactor the code with promises to the new syntax with async and await

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -7,38 +7,80 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const user = new User(req.body);
 
-    user.save()
-        .then(() => {
-            res.status(201).send(user);
-        })
-        .catch((error) => {
-            res.status(400).send(error);
-        });
+    try {
+        await user.save();
+        res.status(201).send(user);
+    } catch (error) {
+        res.status(400).send(error);
+    }
 });
 
-app.get('/users', (req, res) => {
-    User.find({})
-        .then(users => {
-            res.send(users);
-        })
-        .catch(error => {
-            res.status(500).send(error);
-        });
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.send(users);
+    } catch (error) {
+        res.status(500).send(error);
+    }
 });
 
-app.post('/tasks', (req, res) => {
+app.get('/users/:id', async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        const user = await User.findById(id);
+
+        if (!user) {
+            return res.status(404).send({
+                error: 'User is not found!'
+            });
+        }
+
+        res.send(user);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+app.post('/tasks', async (req, res) => {
     const task = new Task(req.body);
 
-    task.save()
-        .then(() => {
-            res.status(201).send(task);
-        })
-        .catch(error => {
-            res.status(400).send(error);
-        });
+    try {
+        await task.save();
+        res.status(201).send(task);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+app.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find({});
+        res.send(tasks);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+app.get('/tasks/:id', async (req, res) => {
+    const id = req.params.id;
+    
+    try {
+        const task = await Task.findById(id);
+        
+        if (!task) {
+            return res.status(404).send({
+                error: 'Task is not found!'
+            });
+        }
+
+        res.send(task);
+    } catch (error) {
+        res.status(500).send(error);
+    }
 });
 
 app.listen(3000, () => {
