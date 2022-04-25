c6a42c9de2a0bd954bdb3edc4966bde3daf5e898
Georgi <georgy.keranov@gmail.com>
2022-04-25 18:22:30 +0300

    Create new user in the database for post request '/users'

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
+require('./db/mongoose');
+const User = require('./models/user');
 
 const app = express();
 
+app.use(express.json());
+
 app.post('/users', (req, res) => {
-    res.send('working!');
+    const user = new User(req.body);
+
+    user.save()
+        .then(() => {
+            res.send(user);
+        })
+        .catch((error) => {
+            res.status(400).send(error);
+        });
 });
 
 app.listen(3000, () => {
