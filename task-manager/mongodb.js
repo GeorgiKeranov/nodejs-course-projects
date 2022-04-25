cee4d722ca002a7638312fbfaf08a1c99a3bd594
Georgi <georgy.keranov@gmail.com>
2022-04-25 12:02:22 +0300

    Test the updateMany method

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -14,21 +14,12 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     const db = client.db(databaseName);
     const tasksCollection = db.collection('tasks');
 
-    tasksCollection.findOne({ _id: new ObjectId('62664e14c8e24e33f9d05afc') }, (error, task) => {
-        if (error) {
-            console.log(error);
-            return;
-        }
-
-        console.log(task);
-    });
-
-    tasksCollection.find({ completed: false }).toArray((error, tasks) => {
-        if (error) {
-            console.log(error);
-            return;
-        }
-
-        console.log(tasks);
-    });
+    tasksCollection.updateMany(
+        { completed: false },
+        { $set: { completed: true } }
+    ).then((result) => {
+        console.log(result);
+    }).catch((error) => {
+        console.log(error);
+    })
 });
