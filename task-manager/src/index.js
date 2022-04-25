ef46097e959e7c6637ba60336eb5752186735317
Georgi <georgy.keranov@gmail.com>
2022-04-25 18:29:55 +0300

    Add post '/tasks' endpoint to add new task

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('./db/mongoose');
 const User = require('./models/user');
+const Task = require('./models/task');
 
 const app = express();
 
@@ -11,7 +12,19 @@ app.post('/users', (req, res) => {
 
     user.save()
         .then(() => {
-            res.send(user);
+            res.status(201).send(user);
+        })
+        .catch((error) => {
+            res.status(400).send(error);
+        });
+});
+
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body);
+
+    task.save()
+        .then(() => {
+            res.status(201).send(task);
         })
         .catch((error) => {
             res.status(400).send(error);
